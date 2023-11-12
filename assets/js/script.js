1d0484b51fa5bf1ca6732784d4032cdd6a24b61a
jonatas1516 <jonatas1516@gmail.com>
2023-11-12 19:21:57 -0300

    update JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
+//Declareção das variaveis selecionado as classe do HTML
 let form = document.querySelector(".formulario")
 let mask = document.querySelector(".mask-form")
 
+//Função do Botão da section PRINCIPAL
 function cliqueNoBotao() {
   form.style.left = "40%"
   mask.style.visibility = "visible"
 }
 
+//Função do Botão da section FORM
 function sumirForm(){
   form.style.display = "-340px"
   mask.style.visibility = "hidden"
